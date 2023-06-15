d509e31f3756327f80a108f85abb4648b64e9759
Mahmoud Agbaria <mahmoud.moaad@gmail.com>
2023-06-15 23:13:25 +0300

    implement viewing other users profiles functionality

---
+++ app/profile/[id]/page.jsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+import Profile from '@components/Profile';
+
+const UserProfile = ({ params }) => {
+
+    const searchParams = useSearchParams();
+    const userName = searchParams.get("name");
+
+    const [userPosts, setUserPosts] = useState([]);
+
+    useEffect(() => {
+        const getProfileDetails = async () => {
+            const response = await fetch(`/api/users/${params?.id}/posts`);
+            const data = await response.json();
+            setUserPosts(data);
+
+
+        };
+
+        if (params?.id) getProfileDetails();
+    }, [params.id]);
+
+    return (
+        <Profile
+            name={userName}
+            desc={`Welcome to ${userName}'s personalized profile page. Explore ${userName}'s exceptional prompts and be inspired by the power of their imagination`}
+            data={userPosts}
+        />
+    );
+}
+
+export default UserProfile
+
+
+
